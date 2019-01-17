a59a8f1fbb14c699867f8c090a78346783fe0b40
wiebeJPX <wiebe@justpremium.com>
2019-01-17 13:33:05 +0100

    latest fixes

--- js/verhaal.js
+++ js/verhaal.js
@@ -1,14 +1,14 @@
-// Nav Scroll
-var titelEl = document.querySelector('.titlebox');
-var mijnLijst = document.querySelector('a.mijn-lijst');
-var lijstIcon = document.querySelector('.lijst-icon');
-
-window.top.addEventListener("scroll", function(){
- if (window.top.pageYOffset > 500){
-    titelEl.classList.add('hidetitle');
-
-} else if (window.top.pageYOffset < 500){
-    titelEl.classList.remove('hidetitle');
-
-    }
-})
+// // Nav Scroll
+// var titelEl = document.querySelector('.titlebox');
+// var mijnLijst = document.querySelector('a.mijn-lijst');
+// var lijstIcon = document.querySelector('.lijst-icon');
+//
+// window.top.addEventListener("scroll", function(){
+//  if (window.top.pageYOffset > 500){
+//     titelEl.classList.add('hidetitle');
+//
+// } else if (window.top.pageYOffset < 500){
+//     titelEl.classList.remove('hidetitle');
+//
+//     }
+// })
